94992ec0a13d17a5d68f05fc3a5af867883ea24c
unknown <junfan62@126.com>
2017-07-25 19:16:09 +0800

    Initial commit8

---
+++ src/com/youmeek/demo/HelloWord.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.print("Hello word");
+    }
+}
